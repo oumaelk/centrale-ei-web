f890f0b944797940bd0d3891736dabd71b9cb5e7
Ouma <oumayma.el-khattabi@student-cs.fr>
2022-06-08 14:46:50 +0200

    get db

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>My Films Recommendation</h1>
     <p>Movie Name: {{ $data.movieName }}</p>
-    <input type="text" v-model="movieName" placeholder="Search movies"/>
+    <input type="text" v-model="movieName" placeholder="Search movies" />
     <h2>Most Popular this week</h2>
     <div class="parent">
       <li v-for="movie in filteredMovies" :key="movie.id">
@@ -40,46 +40,22 @@ export default {
   methods: {
     fetchMovies: function () {
       axios
-        .get(
-          "https://api.themoviedb.org/3/movie/popular?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&language=en-US&page=1"
-        )
-        .then((response) => {
-          this.movies = response.data.results;
-        });
+        .get(`localhost:3000/movies/`)
+        .then((response) => (this.movies = response));
     },
-    /*fetchRecommentations: function (movieId) {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/movie/" +
-            movieId +
-            "/recommendations?api_key=522d421671cf75c2cba341597d86403a&language=en-US&page=1"
-        )
-        .then((response) => {
-          console.log(response);
-          this.recommanded[movieId] = response.data.results;
-        });
-    },
-    search: function () {
-      for (const movie in this.movies) {
-        if (movie.title.includes(this.movieName)) {
-          this.match_search.push(movie);
-        }
-      }
-    },*/
   },
   mounted: function () {
     this.fetchMovies();
-    for (const movie in this.movies) {
-      this.fetchRecommentations(movie.id);
-    }
   },
-  computed:{
-    filteredMovies: function(){
-      return this.movies.filter((movie)=>{
-        return movie.title.toLowerCase().match(this.movieName.toLocaleLowerCase());
+  computed: {
+    filteredMovies: function () {
+      return this.movies.filter((movie) => {
+        return movie.title
+          .toLowerCase()
+          .match(this.movieName.toLocaleLowerCase());
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
