5fe1ef244cf2f2a8bf4a5522b928d5cfe9e8b208
Antonia Raquiman <antonia.raquiman@ing.uchile.cl>
2022-06-10 11:41:10 +0200

    avance

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,3 +1,7 @@
 <template>
-  <h2>This is an about page</h2>
+  <h2>About us</h2>
+  <p>
+    We are a group of three women developping a movie recommendation website for
+    our class.
+  </p>
 </template>
